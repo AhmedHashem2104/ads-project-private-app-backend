c69f7b5bf070c6340809d8d9c479d34cbc1a6fb8
AhmedHashem2104 <ahmedhashem@qodeex.com>
2021-04-13 02:55:52 +0200

    v2.1

--- app/Controllers/Http/AdsController.ts
+++ app/Controllers/Http/AdsController.ts
@@ -1,19 +1,13 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class AdsController {
-  public async index ({}: HttpContextContract) {
-  }
-
-  public async create ({}: HttpContextContract) {
+  public async index ({ response }: HttpContextContract) {
   }
 
   public async store ({}: HttpContextContract) {
   }
 
   public async show ({}: HttpContextContract) {
-  }
-
-  public async edit ({}: HttpContextContract) {
   }
 
   public async update ({}: HttpContextContract) {
